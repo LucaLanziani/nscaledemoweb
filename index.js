58a417eb00d8795224f23f05bea71d585b3cb351
Luca Lanziani <luca@lanziani.com>
2015-05-29 12:16:20 +0200

    Esclamation point

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 8000;
 
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
+  response.end('Hello World!\n');
 }).listen(port, function() {
   console.log('Server started on port', port);
 });
